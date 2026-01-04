06145e5adaf7d63befb517566486e56d68817ed1
Ailing Zhang <ailzhang@users.noreply.github.com>
2026-01-03 18:46:14 -0800

    permission

--- nvim/init.lua
+++ nvim/init.lua
@@ -240,7 +240,7 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
             vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
             vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+            vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, opts)
           end,
         })
       end,
